fa8294f40d9c6ae427c1c1e7336307c2e3f3be5d
Scoutzknifez <cody@4alexanders.com>
2019-10-26 22:30:32 -0700

    Added the buyer account,still needs to be discussed.

---
+++ src/com/CS3560Project/structures/BuyerAccount.java
@@ -0,0 +1,7 @@
+package com.CS3560Project.structures;
+
+public class BuyerAccount extends User {
+    public BuyerAccount(String id, String firstName, String lastName, PhoneNumber phoneNumber, String address, String email, String password) {
+        super(id, firstName, lastName, phoneNumber, address, email, password);
+    }
+}
