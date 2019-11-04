b219999d791e055bf6855d0bb0f8159b6730bb99
Scoutzknifez <Cody@4alexanders.com>
2019-11-03 19:31:33 -0800

    Removed the added tables because they are not ready.

--- src/com/CS3560Project/sqlworkers/Table.java
+++ src/com/CS3560Project/sqlworkers/Table.java
@@ -1,16 +1,18 @@
 package com.CS3560Project.sqlworkers;
 
 import com.CS3560Project.structures.User;
-import com.CS3560Project.structures.products.Product;
-import com.CS3560Project.structures.products.ProductReview;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * Anything inside here must have a createInstance method with a ResultSet as a parameter.
+ */
 @Getter
 @AllArgsConstructor
 public enum Table {
-    PRODUCT(Product.class),
-    PRODUCT_REVIEWS(ProductReview.class),
+    //PRODUCT(Product.class),
+    //PRODUCT_IMAGES(ProductImages.class),
+    //PRODUCT_REVIEWS(ProductReview.class),
     USERS(User.class);
 
     private Class constructorClass;
