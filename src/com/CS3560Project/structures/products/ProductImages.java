2a04e4f575cae4998b64a80e9f3ccb325dea7d20
Scoutzknifez <Cody@4alexanders.com>
2019-11-08 18:39:23 -0800

    Added createInstance method for ProductImages, needs to be created still.

--- src/com/CS3560Project/structures/products/ProductImages.java
+++ src/com/CS3560Project/structures/products/ProductImages.java
@@ -1,10 +1,13 @@
 package com.CS3560Project.structures.products;
 
+import com.CS3560Project.exceptions.ParseFailureException;
+import com.CS3560Project.utility.Utils;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.awt.Image;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,5 +25,15 @@ public class ProductImages {
 
     public void addImage(Image image) {
         imageList.add(image);
+    }
+
+    public static ProductImages createInstance(ResultSet set) {
+        try {
+            // TODO Decode the base64 strings given back here and put into list
+            return null;
+        } catch (Exception e) {
+            Utils.log("Could not parse returned list.");
+            throw new ParseFailureException(set, ProductImages.class);
+        }
     }
 }
