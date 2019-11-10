77a557216045bfbeefaec1610eee0bee5431388b
Scoutzknifez <cody@4alexanders.com>
2019-11-09 22:03:39 -0800

    Added createInstance for productReview needs to be filled in.

--- src/com/CS3560Project/structures/products/ProductReview.java
+++ src/com/CS3560Project/structures/products/ProductReview.java
@@ -1,7 +1,11 @@
 package com.CS3560Project.structures.products;
 
+import com.CS3560Project.exceptions.ParseFailureException;
+import com.CS3560Project.utility.Utils;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.sql.ResultSet;
 
 @Getter
 @AllArgsConstructor
@@ -12,5 +16,16 @@ public class ProductReview {
     @Override
     public String toString() {
         return "{rating:" + getRating() + ",review:\"" + getReview() + "\"}";
+    }
+
+    public static ProductReview createInstance(ResultSet set) {
+        try {
+            // TODO Look at user for how to fill this in.
+            // set field names are in discord
+            return null;
+        } catch (Exception e) {
+            Utils.log("Could not parse returned list.");
+            throw new ParseFailureException(set, ProductReview.class);
+        }
     }
 }
