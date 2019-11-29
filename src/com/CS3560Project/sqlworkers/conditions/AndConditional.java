5b8d052dcf81d047bb9e51734da1697a07429765
Scoutzknifez <cody@4alexanders.com>
2019-11-28 23:15:46 -0800

    And 2 conditional statements to MySQL queries, more to come

--- src/com/CS3560Project/sqlworkers/conditions/AndConditional.java
+++ src/com/CS3560Project/sqlworkers/conditions/AndConditional.java
@@ -1,4 +1,16 @@
 package com.CS3560Project.sqlworkers.conditions;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class AndConditional {
+    private Conditional condition1;
+    private Conditional condition2;
+
+    @Override
+    public String toString() {
+        return "(" + getCondition1().toString() + " AND " + getCondition2().toString() + ")";
+    }
 }
--- src/com/CS3560Project/sqlworkers/conditions/OrConditional.java
+++ src/com/CS3560Project/sqlworkers/conditions/OrConditional.java
@@ -1,4 +1,16 @@
 package com.CS3560Project.sqlworkers.conditions;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class OrConditional {
+    private Conditional condition1;
+    private Conditional condition2;
+
+    @Override
+    public String toString() {
+        return "(" + getCondition1().toString() + " OR " + getCondition2().toString() + ")";
+    }
 }
