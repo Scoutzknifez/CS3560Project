7bae7c8d0f2ed3613346d5358ae42fce23bd8b23
Shannon <33528736+slinky102@users.noreply.github.com>
2019-12-03 18:27:19 -0800

    Edited search function to populate gridPane

--- src/com/CS3560Project/GUI/ProductView.java
+++ src/com/CS3560Project/GUI/ProductView.java
@@ -1,24 +1,48 @@
 package com.CS3560Project.GUI;
 
 import com.CS3560Project.structures.products.*;
+import com.CS3560Project.utility.Utils;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
+import java.util.ArrayList;
+import java.util.List;
+
+//How to display images
+//Image image = Utils.bufferImageToFXImage(Utils.base64ToBufferImage(product.getProductImage().get(0).getBase64()));
+
 public class ProductView
 {
-    protected ImageView imageView = new ImageView();
+    protected ImageView imageView;
     protected Button add = new Button("+");
     protected VBox vbox = new VBox(10, imageView, add);
 
-    public ProductView(Image image)
+    public ProductView(Product product)
     {
-        imageView = new ImageView(image);
+        imageView = new ImageView(Utils.bufferedImageToFXImage(Utils.base64ToBufferedImage(product.getProductImages().get(0).getBase64())));
+    }
+
+    public static List<ProductView> createProductViews(List<Product> products)
+    {
+        List<ProductView> views = new ArrayList<>();
+        for(int i = 0; i < products.size(); i++)
+        {
+            views.add(new ProductView(products.get(i)));
+        }
+        return views;
     }
 
     public VBox show()
     {
         return vbox;
     }
+
+    public ImageView getImageView()
+    {
+        return imageView;
+    }
+
 }
