71d71936bfa91cb44628b07caf86d061761035e8
Shannon <33528736+slinky102@users.noreply.github.com>
2019-12-04 15:16:13 -0800

    Shopping Cart saves amount in cart when switching windows. Logout Label added.

--- src/com/CS3560Project/GUIMain.java
+++ src/com/CS3560Project/GUIMain.java
@@ -26,6 +26,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -39,7 +40,7 @@ public class GUIMain extends Application {
     protected List<Product> searchResults; // TODO Needs to be fetched from active inventories - Cody (This is something I will take part in on Monday)
     protected List<ProductView> images = new ArrayList<>(); // TODO This aint right - Cody (Products own images themselves)
     protected static Cart cart = null;
-    protected Stage primaryStage;
+    protected Stage primaryStage = new Stage();
     protected GridPane shoppingList = new GridPane();
     public Label shoppingCartLabel;
 
@@ -59,74 +60,11 @@ public class GUIMain extends Application {
         TextField search = new TextField();
         HBox hboxSearch = new HBox(search, searchButton);
 
-        Button add1 = new Button("+");
-        Button add2 = new Button("+");
-        Button add3 = new Button("+");
-        Button add4 = new Button("+");
-        Button add5 = new Button("+");
-        Button add6 = new Button("+");
-
-        Button sub1 = new Button("-");
-        Button sub2 = new Button("-");
-        Button sub3 = new Button("-");
-        Button sub4 = new Button("-");
-        Button sub5 = new Button("-");
-        Button sub6 = new Button("-");
-
-        Label quantity1 = new Label("0");
-        Label quantity2 = new Label("0");
-        Label quantity3 = new Label("0");
-        Label quantity4 = new Label("0");
-        Label quantity5 = new Label("0");
-        Label quantity6 = new Label("0");
-
-        //Hboxes and Vboxes to create shopping menu
-       /* HBox hbox1 = new HBox(10, add1, quantity1, sub1);
-        HBox hbox2 = new HBox(10, add2, quantity2, sub2);
-        HBox hbox3 = new HBox(10, add3, quantity3, sub3);
-        HBox hbox4 = new HBox(10, add4, quantity4, sub4);
-        HBox hbox5 = new HBox(10, add5, quantity5, sub5);
-        HBox hbox6 = new HBox(10, add6, quantity6, sub6);
-
-        hbox1.setPadding(new Insets(10));
-        hbox2.setPadding(new Insets(10));
-        hbox3.setPadding(new Insets(10));
-        hbox4.setPadding(new Insets(10));
-        hbox5.setPadding(new Insets(10));
-        hbox6.setPadding(new Insets(10));
-
-        hbox1.setAlignment(Pos.CENTER);
-        hbox2.setAlignment(Pos.CENTER);
-        hbox3.setAlignment(Pos.CENTER);
-        hbox4.setAlignment(Pos.CENTER);
-        hbox5.setAlignment(Pos.CENTER);
-        hbox6.setAlignment(Pos.CENTER);
-
-        VBox vbox1 = new VBox(item1.getImageView(), hbox1);
-        VBox vbox2 = new VBox(item2.getImageView(), hbox2);
-        VBox vbox3 = new VBox(item3.getImageView(), hbox3);
-        VBox vbox4 = new VBox(item4.getImageView(), hbox4);
-        VBox vbox5 = new VBox(item5.getImageView(), hbox5);
-        VBox vbox6 = new VBox(item6.getImageView(), hbox6);
-
-        vbox1.setAlignment(Pos.CENTER);
-        vbox2.setAlignment(Pos.CENTER);
-        vbox3.setAlignment(Pos.CENTER);
-        vbox4.setAlignment(Pos.CENTER);
-        vbox5.setAlignment(Pos.CENTER);
-        vbox6.setAlignment(Pos.CENTER);
-
-        shoppingList.add(vbox1, 0, 0);
-        shoppingList.add(vbox2, 1, 0);
-        shoppingList.add(vbox3, 2, 0);
-        shoppingList.add(vbox4, 0, 1);
-        shoppingList.add(vbox5, 1, 1);
-        shoppingList.add(vbox6, 2, 1);*/
-
         shoppingList.setAlignment(Pos.CENTER);
         shoppingList.setPadding(new Insets(10,10,10,10));
 
-        Label cart = new Label("Shopping Cart (0)");
+        Label logout = new Label("Logout");
+        Label cart = new Label("Shopping Cart (" + ProductView.count + ")");
         shoppingCartLabel = cart;
         cart.addEventHandler(MouseEvent.MOUSE_CLICKED, event ->{
             try
@@ -141,99 +79,21 @@ public class GUIMain extends Application {
 
         });
 
-        HBox hbox = new HBox(50, hboxSearch, cart);
+        logout.setOnMouseClicked(event ->{
+           Alert sure = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to logout?");
+           Optional<ButtonType> result = sure.showAndWait();
+           if(result.isPresent() && result.get() == ButtonType.OK)
+           {
+               login();
+           }
+        });
+
+        HBox hbox = new HBox(75, hboxSearch, cart, logout);
         hbox.setAlignment(Pos.CENTER);
 
         borderpane.setTop(hbox);
         borderpane.setCenter(shoppingList);
         borderpane.setPadding(new Insets(10,10,10,10));
-
-        //Action Events
-        //Adding to shopping cart; updates quantity under item
-        //May be able to bind data to labels later, if time permits. Will work with repeated overwrite for now
-        /*add1.setOnAction(event -> {
-            count1++;
-            totalCount++;
-            quantity1.setText("" + count1);
-            cart.setText("Shopping Cart (" + totalCount + ")");
-        });
-
-        add2.setOnAction(event -> {
-            count2++;
-            totalCount++;
-            quantity2.setText("" + count2);
-            cart.setText("Shopping Cart (" + totalCount + ")");
-        });
-
-        add3.setOnAction(event -> {
-            count3++;
-            totalCount++;
-            quantity3.setText("" + count3);
-            cart.setText("Shopping Cart (" + totalCount + ")");
-        });
-
-        add4.setOnAction(event -> {
-            count4++;
-            totalCount++;
-            quantity4.setText("" + count4);
-            cart.setText("Shopping Cart (" + totalCount + ")");
-        });
-
-        add5.setOnAction(event -> {
-            count5++;
-            totalCount++;
-            quantity5.setText("" + count5);
-            cart.setText("Shopping Cart (" + totalCount + ")");
-        });
-
-        add6.setOnAction(event -> {
-            count6++;
-            totalCount++;
-            quantity6.setText("" + count6);
-            cart.setText("Shopping Cart (" + totalCount + ")");
-        });
-
-        //Removes from shopping cart; updates quantity under item
-        sub1.setOnAction(event -> {
-            count1--;
-            totalCount--;
-            quantity1.setText("" + count1);
-            cart.setText("Shopping Cart (" + totalCount + ")");
-        });
-
-        sub2.setOnAction(event -> {
-            count2--;
-            totalCount--;
-            quantity2.setText("" + count2);
-            cart.setText("Shopping Cart (" + totalCount + ")");
-        });
-        sub3.setOnAction(event -> {
-            count3--;
-            totalCount--;
-            quantity3.setText("" + count3);
-            cart.setText("Shopping Cart (" + totalCount + ")");
-        });
-
-        sub4.setOnAction(event -> {
-            count4--;
-            totalCount--;
-            quantity4.setText("" + count4);
-            cart.setText("Shopping Cart (" + totalCount + ")");
-        });
-
-        sub5.setOnAction(event -> {
-            count5--;
-            totalCount--;
-            quantity5.setText("" + count5);
-            cart.setText("Shopping Cart (" + totalCount + ")");
-        });
-
-        sub6.setOnAction(event -> {
-            count6--;
-            totalCount--;
-            quantity6.setText("" + count6);
-            cart.setText("Shopping Cart (" + totalCount + ")");
-        });*/
 
         //Read text field for given search term to decide on chosen items
         search.setOnKeyPressed(event ->{
@@ -253,7 +113,7 @@ public class GUIMain extends Application {
             }
         });
 
-        Scene scene = new Scene(borderpane,900,800);
+        Scene scene = new Scene(borderpane,1000,600);
         //scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
         primaryStage = new Stage();
         primaryStage.setTitle("Online Shopping Network");
@@ -388,12 +248,13 @@ public class GUIMain extends Application {
 
         //makes the rows for items
         ArrayList<Product> addedItems = new ArrayList<Product>();
-        for(Product product: cart.getCartItems()) {
+        //TODO: Fix this to properly populate the shopping cart page
+        /*for(Product product: cart.getCartItems()) {
             if(!addedItems.contains(product)) {
                 addedItems.add(product);
                 list.getChildren().add(makeItem(product));
             }
-        }
+        }*/
         list.setAlignment(Pos.BASELINE_CENTER);
 
 
@@ -421,6 +282,7 @@ public class GUIMain extends Application {
     }
 
     private void login (){
+        primaryStage.close();
         primaryStage = new Stage();
         primaryStage.setTitle("Login to MarketPlace");
         primaryStage.setMinHeight(400);
